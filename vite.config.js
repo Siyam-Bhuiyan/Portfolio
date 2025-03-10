5b849a26e161b2d88d0107aed835fd48972d04c5
Siyam <bhuiyansiyam294@gmail.com>
2025-03-10 23:54:39 +0600

    Update dependencies and enhance Vite configuration for improved build performance

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,38 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
   base: "/Portfolio/",
+  plugins: [react()],
+  build: {
+    chunkSizeWarningLimit: 1500, // Increase limit further
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          vendor: ["react", "react-dom"],
+          three: ["three"],
+          "react-three": ["@react-three/fiber", "@react-three/drei"],
+          utils: [
+            "framer-motion",
+            "maath",
+            "react-router-dom",
+            "react-tilt",
+            "react-vertical-timeline-component",
+          ],
+        },
+        assetFileNames: (assetInfo) => {
+          if (
+            assetInfo.name.endsWith(".png") ||
+            assetInfo.name.endsWith(".jpg")
+          ) {
+            return "assets/images/[name]-[hash][extname]";
+          }
+          return "assets/[name]-[hash][extname]";
+        },
+      },
+    },
+    assetsInlineLimit: 4096, // 4kb
+    target: "esnext",
+    minify: "esbuild",
+  },
 });
