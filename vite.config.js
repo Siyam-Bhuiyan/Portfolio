a03ed89d68a0df80bb8b70e92d4f1e3fa053d1ad
Siyam <bhuiyansiyam294@gmail.com>
2025-03-11 03:37:18 +0600

    Enhance lighting in Computers component and refine Vercel configuration for better build performance

--- vite.config.js
+++ vite.config.js
@@ -4,14 +4,21 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
   plugins: [react()],
   build: {
+    chunkSizeWarningLimit: 1000,
     rollupOptions: {
       output: {
-        manualChunks: {
-          vendor: ["react", "react-dom"],
-          three: ["three"],
-          "react-three": ["@react-three/fiber", "@react-three/drei"],
-        }
-      }
-    }
-  }
+        manualChunks(id) {
+          if (id.includes("node_modules")) {
+            if (id.includes("three")) {
+              return "three";
+            }
+            if (id.includes("@react-three")) {
+              return "react-three";
+            }
+            return "vendor";
+          }
+        },
+      },
+    },
+  },
 });
