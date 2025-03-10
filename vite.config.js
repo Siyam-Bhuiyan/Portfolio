6c11c851c6b57e09ef5c85df0c61dc50db309f69
Siyam <bhuiyansiyam294@gmail.com>
2025-03-11 00:28:03 +0600

    Fix asset paths in index.html and update Vercel configuration for Vite framework

--- vite.config.js
+++ vite.config.js
@@ -2,37 +2,16 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
-  base: "./",
   plugins: [react()],
   build: {
-    chunkSizeWarningLimit: 1500, // Increase limit further
     rollupOptions: {
       output: {
         manualChunks: {
           vendor: ["react", "react-dom"],
           three: ["three"],
           "react-three": ["@react-three/fiber", "@react-three/drei"],
-          utils: [
-            "framer-motion",
-            "maath",
-            "react-router-dom",
-            "react-tilt",
-            "react-vertical-timeline-component",
-          ],
-        },
-        assetFileNames: (assetInfo) => {
-          if (
-            assetInfo.name.endsWith(".png") ||
-            assetInfo.name.endsWith(".jpg")
-          ) {
-            return "assets/images/[name]-[hash][extname]";
-          }
-          return "assets/[name]-[hash][extname]";
-        },
-      },
-    },
-    assetsInlineLimit: 4096, // 4kb
-    target: "esnext",
-    minify: "esbuild",
-  },
+        }
+      }
+    }
+  }
 });
