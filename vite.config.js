48be9322d5e6585944dd5ff49d253a8359b693a8
Siyam <bhuiyansiyam294@gmail.com>
2025-03-11 00:22:59 +0600

    Update base href in index.html and adjust Vercel configuration for improved routing

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,7 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
-  base: "/Portfolio/",
+  base: "./",
   plugins: [react()],
   build: {
     chunkSizeWarningLimit: 1500, // Increase limit further
