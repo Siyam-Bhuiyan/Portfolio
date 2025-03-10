c2091e6a18ac31776929dc5e6802d2ff781f263f
Siyam <bhuiyansiyam294@gmail.com>
2025-03-11 03:45:25 +0600

    Refactor Vercel configuration and optimize chunking in Vite setup

--- vite.config.js
+++ vite.config.js
@@ -4,21 +4,14 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
   plugins: [react()],
   build: {
-    chunkSizeWarningLimit: 1000,
     rollupOptions: {
       output: {
-        manualChunks(id) {
-          if (id.includes("node_modules")) {
-            if (id.includes("three")) {
-              return "three";
-            }
-            if (id.includes("@react-three")) {
-              return "react-three";
-            }
-            return "vendor";
-          }
-        },
-      },
-    },
-  },
+        manualChunks: {
+          vendor: ["react", "react-dom"],
+          three: ["three"],
+          "react-three": ["@react-three/fiber", "@react-three/drei"],
+        }
+      }
+    }
+  }
 });
